2a11bfbbc5dabef78099af46eb19b05314273293
Sun Xinyu <sunxinyu347@gmail.com>
2024-05-15 17:17:15 +0800

    test

---
+++ target/debug/deps/yx_rust-c01733d33442ca86.d
@@ -0,0 +1,6 @@
+/Users/sunxinyu/develop/yx_rust/YXCHF/target/debug/deps/libyx_rust-c01733d33442ca86.rmeta: src/main.rs src/mole.rs
+
+/Users/sunxinyu/develop/yx_rust/YXCHF/target/debug/deps/yx_rust-c01733d33442ca86.d: src/main.rs src/mole.rs
+
+src/main.rs:
+src/mole.rs:
